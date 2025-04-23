c336174f67058dbd5be7b849160166fee16c70aa
pmbittner <paul.bittner@uni-ulm.de>
2025-04-23 16:15:43 +0200

    Comicify: configurable contrast value

--- src/main/java/de/bittner/colourkiste/workspace/commands/Comicify.java
+++ src/main/java/de/bittner/colourkiste/workspace/commands/Comicify.java
@@ -4,11 +4,19 @@ import de.bittner.colourkiste.rendering.Texture;
 import de.bittner.colourkiste.imageprocessing.kernels.Erosion;
 
 public class Comicify extends UndoableTextureManipulation {
+    private int contrastValue = 120;
+
+    public Comicify(int contrastValue) {
+        this.contrastValue = contrastValue;
+    }
+
+    public Comicify() {}
+
     public void manipulate(Texture texture) {
         Erosion erode = Erosion.Erosion();
         Erosion dilate = Erosion.Dilatation();
 
-        Texture bw = texture.contrast(120);
+        Texture bw = texture.contrast(this.contrastValue);
 
         bw = erode.apply(bw);
         bw = erode.apply(bw);
